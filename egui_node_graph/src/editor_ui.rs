7fb766b3467f158c4224c9e5921aab0e71d85bcf
obulka <obulka@hotmail.com>
2024-05-05 16:11:26 -0700

    updated egui to 0.26.2

--- egui_node_graph/src/editor_ui.rs
+++ egui_node_graph/src/editor_ui.rs
@@ -117,12 +117,11 @@ where
         // (so for windows it will use up to the resizeably set limit
         // and for a Panel it will fill it completely)
         let editor_rect = ui.max_rect();
-        let resp = ui.allocate_rect(editor_rect, Sense::hover());
 
         let cursor_pos = ui
             .ctx()
             .input(|i| i.pointer.hover_pos().unwrap_or(Pos2::ZERO));
-        let mut cursor_in_editor = resp.hovered();
+        let mut cursor_in_editor = ui.rect_contains_pointer(editor_rect);
         let mut cursor_in_finder = false;
 
         // Gets filled with the node metrics as they are drawn

e7f32e41b2e2aee2e0f1bf477ca066bfdc292037
obulka <obulka@hotmail.com>
2024-05-05 15:22:31 -0700

    updated to egui 0.23.0

--- egui_node_graph/src/editor_ui.rs
+++ egui_node_graph/src/editor_ui.rs
@@ -802,46 +802,46 @@ where
             let titlebar_height = title_height + margin.y;
             let titlebar_rect =
                 Rect::from_min_size(outer_rect.min, vec2(outer_rect.width(), titlebar_height));
-            let titlebar = Shape::Rect(RectShape {
-                rect: titlebar_rect,
+            let titlebar = Shape::Rect(RectShape::new(
+                titlebar_rect,
                 rounding,
-                fill: self.graph[self.node_id]
+                self.graph[self.node_id]
                     .user_data
                     .titlebar_color(ui, self.node_id, self.graph, user_state)
                     .unwrap_or_else(|| background_color.lighten(0.8)),
-                stroke: Stroke::NONE,
-            });
+                Stroke::NONE,
+            ));
 
             let body_rect = Rect::from_min_size(
                 outer_rect.min + vec2(0.0, titlebar_height - rounding_radius),
                 vec2(outer_rect.width(), outer_rect.height() - titlebar_height),
             );
-            let body = Shape::Rect(RectShape {
-                rect: body_rect,
-                rounding: Rounding::none(),
-                fill: background_color,
-                stroke: Stroke::NONE,
-            });
+            let body = Shape::Rect(RectShape::new(
+                body_rect,
+                Rounding::ZERO,
+                background_color,
+                Stroke::NONE,
+            ));
 
             let bottom_body_rect = Rect::from_min_size(
                 body_rect.min + vec2(0.0, body_rect.height() - titlebar_height * 0.5),
                 vec2(outer_rect.width(), titlebar_height),
             );
-            let bottom_body = Shape::Rect(RectShape {
-                rect: bottom_body_rect,
+            let bottom_body = Shape::Rect(RectShape::new(
+                bottom_body_rect,
                 rounding,
-                fill: background_color,
-                stroke: Stroke::NONE,
-            });
+                background_color,
+                Stroke::NONE,
+            ));
 
             let node_rect = titlebar_rect.union(body_rect).union(bottom_body_rect);
             let outline = if self.selected {
-                Shape::Rect(RectShape {
-                    rect: node_rect.expand(1.0),
+                Shape::Rect(RectShape::new(
+                    node_rect.expand(1.0),
                     rounding,
-                    fill: Color32::WHITE.lighten(0.8),
-                    stroke: Stroke::NONE,
-                })
+                    Color32::WHITE.lighten(0.8),
+                    Stroke::NONE,
+                ))
             } else {
                 Shape::Noop
             };
